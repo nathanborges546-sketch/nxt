d953c32333e33754b377313af5cb47cb5f39e894
nathanborges546-sketch <nathanborges546@gmail.com>
2026-01-18 21:32:05 +0100

    Update ContactForm.tsx

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -74,7 +74,7 @@ export function ContactForm() {
 
     setIsSubmitting(true);
     try {
-      const response = await fetch("https://formspree.io/f/xpwzgkqr", {
+      const response = await fetch("https://formspree.io/f/xdaaevre", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
